b283ef5833c19a6b65c61ad2a2251ea09135293d
mayazad <officialmayazad@gmail.com>
2025-07-22 22:27:32 +0600

    Added fourth assignment to assignment-four

---
+++ assignment-four/problem_01.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(me, name, age):
+        me.name = name
+        me.age = age
+        print(f"Name: {me.name}, Age: {me.age}")
+    # def display(me):
+        
+        
+person_details = Person("Alice", 25)
+#obj.display() Output: Name: Alice, Age: 25
